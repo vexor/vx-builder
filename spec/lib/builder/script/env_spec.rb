3c500b7be7e27c07087152d6e1258578a68fb455
Dmitry Galinsky <dima.exe@gmail.com>
2014-02-10 20:32:39 +0400

    Revert "fixed specs"
    This reverts commit b591be4776d9664f8288e40965e7ebc930d43a02.

--- spec/lib/builder/script/env_spec.rb
+++ spec/lib/builder/script/env_spec.rb
@@ -9,4 +9,14 @@ describe Vx::Builder::Script::Env do
 
   it { should eq 0 }
 
+  context "run it" do
+    let(:command) { create :command_from_env, env: env }
+    before { run }
+
+    it "should be success" do
+      system( %{bash -c "#{command}" } )
+      expect($?.to_i).to eq 0
+    end
+  end
+
 end
