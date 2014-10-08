8dfe226c5b5d6a86a8c68cca08df871359af1cac
Dmitry Galinsky <dima.exe@gmail.com>
2014-10-09 03:39:03 +0400

    fixed integration fixtures

--- spec/fixtures/integration/ruby/matrix/1.after_script.sh
+++ spec/fixtures/integration/ruby/matrix/1.after_script.sh
@@ -9,6 +9,7 @@ export CI_PROJECT_NAME=vexor/vx-test-repo
 export CI_BUILD_SHA=8f53c077072674972e21c82a286acc07fada91f5
 export CI_BRANCH=test/pull-request
 export VX_ROOT=$(pwd)
+test -d ${VX_ROOT}/code/vexor/vx-test-repo || exit 1
 cd ${VX_ROOT}/code/vexor/vx-test-repo
 
 # after script
