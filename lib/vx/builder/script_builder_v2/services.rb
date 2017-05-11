ada2a1e741d02ef6a8489da944b4bff81530c3eb
Alexander Kirillov <saratovsource@gmail.com>
2017-05-11 19:47:19 +0400

    Filter new services semantic (exclude from script builder)

--- lib/vx/builder/script_builder_v2/services.rb
+++ lib/vx/builder/script_builder_v2/services.rb
@@ -10,13 +10,25 @@ module Vx
 
         def call(env)
           env.stage("init").tap do |e|
-            srvs = env.source.services.map do |srv|
-              srv = ALIASES[srv] || srv
-            end
+            srvs = env.source.services
+                     .select{ |srv| legacy_service?(srv) }
+                     .map{ |srv| srv = ALIASES[srv] || srv }
             e.add_task "services", srvs
           end
 
           app.call(env)
+        end
+
+        private
+
+        # If service is array - it is new service, skip it
+        def legacy_service?(service)
+          case service
+          when Array, Hash
+            return false
+          else
+            return true
+          end
         end
 
       end
